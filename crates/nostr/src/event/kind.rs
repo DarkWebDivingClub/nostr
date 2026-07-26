d4791748d66c887092147346d7bcd6dec0a9cbc1
Yuki Kishimoto <yukikishimoto@protonmail.com>
2026-07-26 23:36:11 +0200

    nostr: remove NIP90-related kind constants and methods
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- crates/nostr/src/event/kind.rs
+++ crates/nostr/src/event/kind.rs
@@ -15,10 +15,6 @@ use serde::ser::{Serialize, Serializer};
 
 use crate::error::Error;
 
-/// NIP90 - Job request range
-pub const NIP90_JOB_REQUEST_RANGE: Range<u16> = 5_000..6_000;
-/// NIP90 - Job result range
-pub const NIP90_JOB_RESULT_RANGE: Range<u16> = 6_000..7_000;
 /// Regular range
 pub const REGULAR_RANGE: Range<u16> = 1_000..10_000;
 /// Replaceable range
@@ -277,22 +273,6 @@ impl Kind {
     #[inline]
     pub fn is_addressable(&self) -> bool {
         ADDRESSABLE_RANGE.contains(&self.as_u16())
-    }
-
-    /// Check if it's a NIP90 job request
-    ///
-    /// <https://github.com/nostr-protocol/nips/blob/master/90.md>
-    #[inline]
-    pub fn is_job_request(&self) -> bool {
-        NIP90_JOB_REQUEST_RANGE.contains(&self.as_u16())
-    }
-
-    /// Check if it's a NIP90 job result
-    ///
-    /// <https://github.com/nostr-protocol/nips/blob/master/90.md>
-    #[inline]
-    pub fn is_job_result(&self) -> bool {
-        NIP90_JOB_RESULT_RANGE.contains(&self.as_u16())
     }
 }
 
